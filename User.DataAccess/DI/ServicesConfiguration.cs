dce4cf290b9cad0c3e4dfdf52acaa5e69641f6ad
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 16:49:02 +0300

    feat: add di

---
+++ User.DataAccess/DI/ServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using User.DataAccess.Context;
+
+namespace User.DataAccess.DI;
+
+public static class ServicesConfiguration
+{
+    public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UsersDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+    }
+}
