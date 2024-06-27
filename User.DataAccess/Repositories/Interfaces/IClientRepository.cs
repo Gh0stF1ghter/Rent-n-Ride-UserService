fade0a5da105a574d1adcb1f51843b758dfb4d08
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 13:56:57 +0300

    feat: add repos interface

---
+++ User.DataAccess/Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using User.DataAccess.Entities;
+
+namespace User.DataAccess.Repositories.Interfaces;
+
+public interface IClientRepository : IRepositoryBase<UserEntity>
+{
+    Task<IEnumerable<UserEntity>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+    Task<UserEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ User.DataAccess/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace User.DataAccess.Repositories.Interfaces;
+
+public interface IRepositoryBase<TEntity> where TEntity : class
+{
+    IQueryable<TEntity> GetRange(int page, int pageSize);
+
+    IQueryable<TEntity> GetByCondition(Expression<Func<TEntity, bool>> condition);
+
+    Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken);
+
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task UpdateAsync(TEntity newEntity, CancellationToken cancellationToken);
+
+    Task RemoveAsync(TEntity entityToRemove, CancellationToken cancellationToken);
+}
