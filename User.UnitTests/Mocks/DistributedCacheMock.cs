695e9b837cd01ffe8f4db2dd2592c6f2b573ed44
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 22:14:09 +0300

    feat: add mocks

---
+++ User.UnitTests/Mocks/DistributedCacheMock.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+
+namespace Tests.Mocks;
+
+internal class DistributedCacheMock : Mock<IDistributedCache>
+{
+    public void GetDataFromCache(byte[] dataToReturn) =>
+        Setup(dc => dc.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+        .ReturnsAsync(dataToReturn);
+}
