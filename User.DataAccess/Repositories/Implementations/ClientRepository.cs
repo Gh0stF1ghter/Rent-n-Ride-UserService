a6f300444e28d40aa1042af3f99d7cf1d89e3f38
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 20:41:44 +0300

    Feature/add repositories (#4)
    * refactor: rename Context class file
    
    * feat: add repos interface
    
    * feat: add repos implementation
    
    * feat: add di

---
+++ User.DataAccess/Repositories/Implementations/ClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User.DataAccess.Context;
+using User.DataAccess.Entities;
+using User.DataAccess.Repositories.Interfaces;
+
+namespace User.DataAccess.Repositories.Implementations;
+
+public class ClientRepository(UsersDbContext context) : RepositoryBase<UserEntity>(context), IClientRepository
+{
+    public async Task<IEnumerable<UserEntity>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken) =>
+        await GetRange(page, pageSize)
+            .ToListAsync(cancellationToken);
+
+    public async Task<UserEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
+        await GetByCondition(c => c.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+}
