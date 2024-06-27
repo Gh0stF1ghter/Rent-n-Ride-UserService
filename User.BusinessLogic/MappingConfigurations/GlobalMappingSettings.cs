81cc72e849c9a4b68b7683715f7b716f3a8f1a52
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 13:44:06 +0300

    feat add mapster configuration

---
+++ User.BusinessLogic/MappingConfigurations/GlobalMappingSettings.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using System.Reflection;
+
+namespace User.BusinessLogic.MappingConfigurations;
+
+public static class GlobalMappingSettings
+{
+    public static void SetMapper()
+    {
+        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+        TypeAdapterConfig.GlobalSettings.Default.MaxDepth(2);
+        TypeAdapterConfig.GlobalSettings.Default.PreserveReference(true);
+    }
+}
