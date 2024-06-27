c6a62f07181874d7fc7d95a8dbf780c6988d2912
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 13:57:07 +0300

    feat: add repos implementation

---
+++ User.DataAccess/Repositories/Implementations/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using User.DataAccess.Context;
+using User.DataAccess.Repositories.Interfaces;
+
+namespace User.DataAccess.Repositories.Implementations;
+
+public class RepositoryBase<TEntity>(UsersDbContext context) : IRepositoryBase<TEntity> where TEntity : class
+{
+    public IQueryable<TEntity> GetRange(int page, int pageSize)
+    {
+        var rowsToSkip = (page - 1) * pageSize;
+
+        return context.Set<TEntity>()
+            .Skip(rowsToSkip)
+            .Take(pageSize)
+            .AsNoTracking();
+    }
+
+    public IQueryable<TEntity> GetByCondition(Expression<Func<TEntity, bool>> condition) =>
+        context.Set<TEntity>()
+        .Where(condition)
+        .AsNoTracking();
+
+    public Task<bool> IsExistsAsync(Expression<Func<TEntity, bool>> condition, CancellationToken cancellationToken) =>
+        context.Set<TEntity>().AnyAsync(condition, cancellationToken);
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        await context.Set<TEntity>().AddAsync(entity, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(TEntity newEntity, CancellationToken cancellationToken)
+    {
+        context.Set<TEntity>().Update(newEntity);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveAsync(TEntity entityToRemove, CancellationToken cancellationToken)
+    {
+        context.Set<TEntity>().Remove(entityToRemove);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
