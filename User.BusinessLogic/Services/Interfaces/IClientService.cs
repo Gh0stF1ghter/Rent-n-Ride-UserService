d44e584271d1cd0c9241a911c25a26b028a5cb42
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-27 13:44:25 +0300

    feat: add service interface

---
+++ User.BusinessLogic/Services/Interfaces/IClientService.cs
@@ -0,0 +1,16 @@
+﻿using User.BusinessLogic.Models;
+
+namespace User.BusinessLogic.Services.Interfaces;
+
+public interface IClientService
+{
+    Task<IEnumerable<ClientModel>> GetRangeAsync(int page, int pageSize, CancellationToken cancellationToken);
+
+    Task<ClientModel> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<ClientModel> AddAsync(ClientModel clientModel, CancellationToken cancellationToken);
+
+    Task<ClientModel> UpdateAsync(ClientModel newClientModel, CancellationToken cancellationToken);
+
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
