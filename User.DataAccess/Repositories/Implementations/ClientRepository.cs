f834c9e9acd265dce68eae403e7f2d63af7e1ee4
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-29 23:17:55 +0300

    feat: update repos remove method to handle entity search

--- User.DataAccess/Repositories/Implementations/ClientRepository.cs
+++ User.DataAccess/Repositories/Implementations/ClientRepository.cs
@@ -14,4 +14,11 @@ public class ClientRepository(UsersDbContext context) : RepositoryBase<UserEntit
     public async Task<UserEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
         await GetByCondition(c => c.Id == id)
             .FirstAsync(cancellationToken);
+
+    public async Task RemoveByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var entity = await GetByIdAsync(id, cancellationToken);
+
+        await RemoveAsync(entity, cancellationToken);
+    }
 }
