2b1e608ec2f9065aedee850b0ce335f112dabd89
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-01 11:11:38 +0300

    feat: add DI

---
+++ User.API/DI/ServicesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+using System.Reflection;
+
+namespace User.API.DI;
+
+public static class ServicesConfiguration
+{
+    public static void AddApiDependencies(this IServiceCollection services)
+    {
+        services.AddAutoValidation();
+    }
+
+    private static void AddAutoValidation(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddFluentValidationAutoValidation();
+    }
+}
