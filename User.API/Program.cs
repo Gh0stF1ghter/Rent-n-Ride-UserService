8df7d6ade0b5665e02485e11853c13936be5efc2
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-01 11:18:41 +0300

    feat: add Auth0 support

--- User.API/Program.cs
+++ User.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddApplicationDependencies(builder.Configuration);
-services.AddApiDependencies();
+services.AddApiDependencies(builder.Configuration);
 
 GlobalMappingSettings.SetMapper();
 
