9374416a41af726fb77d00c001f9abe99bfb4693
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-18 12:20:51 +0300

    feat: add grpc service implementation

---
+++ User.BusinessLogic/GrpcServices/ClientGrpcService.cs
@@ -0,0 +1,41 @@
+﻿using ClientGrpcService;
+using Grpc.Core;
+using Mapster;
+using User.BusinessLogic.Services.Interfaces;
+
+namespace User.BusinessLogic.GrpcServices;
+
+public class ClientGrpcService(IClientService service) : ClientService.ClientServiceBase
+{
+    public override async Task<GetClientsInRangeResponse> GetClients(GetClientsInRangeRequest request, ServerCallContext context)
+    {
+        var dataList = await service.GetRangeAsync(request.PageNumber, request.PageSize, context.CancellationToken);
+
+        var responseData = dataList.Adapt<IEnumerable<ProtoClientModel>>();
+
+        var response = new GetClientsInRangeResponse();
+
+        response.Clients.AddRange(responseData);
+
+        return response;
+    }
+
+    public override async Task<GetClientResponse> GetClient(GetClientRequest request, ServerCallContext context)
+    {
+        var idIsValid = Guid.TryParse(request.Id, out var id);
+
+        if (!idIsValid)
+            throw new InvalidOperationException("Provided id is not GUID");
+
+        var data = await service.GetByIdAsync(id, context.CancellationToken);
+
+        var responseData = data.Adapt<ProtoClientModel>();
+
+        var response = new GetClientResponse
+        {
+            Client = responseData
+        };
+
+        return response;
+    }
+}
