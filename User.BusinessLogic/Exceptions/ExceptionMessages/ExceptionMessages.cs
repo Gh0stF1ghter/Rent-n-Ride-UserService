22b7ceb8b80259e872c831aca390aff33e040c29
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-13 17:29:56 +0300

    feat: add missing token exception

--- User.BusinessLogic/Exceptions/ExceptionMessages/ExceptionMessages.cs
+++ User.BusinessLogic/Exceptions/ExceptionMessages/ExceptionMessages.cs
@@ -4,4 +4,6 @@ internal static class ExceptionMessages
 {
     public static string NotFound(string entityType, Guid guid) =>
         $"{entityType} with id:{guid} was not found";
+
+    public const string TokenMissing = "No token provided from authentication api";
 }
