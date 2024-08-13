e02f65c6bf7878b6562c6a18df79725579a84270
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-13 17:32:13 +0300

    feat: add migrations for props

--- User.DataAccess/Migrations/20240813135058_phonenum-empty.Designer.cs
+++ User.DataAccess/Migrations/20240813135058_phonenum-empty.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using User.DataAccess.Context;
@@ -11,9 +12,11 @@ using User.DataAccess.Context;
 namespace User.DataAccess.Migrations
 {
     [DbContext(typeof(UsersDbContext))]
-    partial class UsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240813135058_phonenum-empty")]
+    partial class phonenumempty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
