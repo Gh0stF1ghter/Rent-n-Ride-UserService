9227e10ffe629bedc03f5f42e12f8678f19df56d
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-14 17:58:02 +0300

    feat: remove nullability from client properties

--- User.BusinessLogic/Models/ClientModel.cs
+++ User.BusinessLogic/Models/ClientModel.cs
@@ -5,11 +5,11 @@ public class ClientModel
     public Guid Id { get; set; }
 
     public string UserId { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string? Password { get; set; }
 
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
     public decimal Balance { get; set; }
     public bool IsRenting { get; set; }
 }
