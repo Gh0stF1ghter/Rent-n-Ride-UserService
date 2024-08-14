fef355a20b47d1e9764a8255ed334034003e5e0b
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-08-14 17:57:12 +0300

    feat: add password validator

--- User.API/Validators/ShortClientViewModelValidator.cs
+++ User.API/Validators/ShortClientViewModelValidator.cs
@@ -17,5 +17,11 @@ public class ShortClientViewModelValidator : AbstractValidator<ShortClientViewMo
 
         RuleFor(c => c.Email)
             .EmailAddress();
+
+        RuleFor(c => c.Password)
+            .MinimumLength(8)
+            .MaximumLength(20)
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]*$")
+            .WithMessage("Weak password. Check for one lowercase, one uppercase, one special character and digit");
     }
 }
